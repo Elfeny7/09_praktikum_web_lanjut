4802ffd696a3073accfd713fc4710aae79d51975
elfeny7 <ikmalfaris50@gmail.com>
2023-04-21 07:50:41 +0700

    Praktikum 9: ORM dengan Relasi

---
+++ app/Models/Mahasiswa.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Mahasiswa extends Model
+{
+    protected $table = "mahasiswas";
+    public $timestamps = false;
+    protected $primaryKey = 'Nim';
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'Nim',
+        'Nama',
+        'Kelas',
+        'Jurusan',
+        'No_Handphone',
+        'Email',
+        'TanggalLahir',
+    ];
+
+    public function kelas()
+    {
+        return $this->belongsTo(Kelas::class);
+    }
+};
